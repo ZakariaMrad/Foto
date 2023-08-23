966563c85bd78af44aa15fb2520b1963768786d5
Zakaria <1839783@cstj.qc.ca>
2023-08-23 12:02:21 -0400

    added bootstrap to app-tauri

--- foto-api/src/app.js
+++ foto-api/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/status', (req, res) => { res.status(200).end(); });
-app.head('/status', (req, res) => { res.status(200).end(); });
+app.get('/status', (request, response) => { response.status(200).end(); });
+app.head('/status', (request, response) => { response.status(200).end(); });
 
 export default app;
