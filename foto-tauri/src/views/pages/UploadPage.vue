cc95b9416b7b2d6033f220ebe62dd72ff7c6494e
KatsuroDev <katsuro.userg@gmail.com>
2023-09-20 15:31:17 -0400

    add UploadPage base
    Création de la page de téléversement
    Ajout d'un contrôle de parcours de fichier
    Ajout d'un affichage grotesque de miniature
    Ajout de fonctionnalités de retirer une miniature de la liste de fichier en cliquant sur la miniature

---
+++ foto-tauri/src/views/pages/UploadPage.vue
@@ -0,0 +1,54 @@
+<template>
+  <DefaultLayout>
+    <div class="d-flex align-center flex-column mt-3">
+      <h1 class="text-h2 text-bold">Téléversement</h1>
+    </div>
+    <div class="w-50 mx-auto image-input">
+      <v-file-input label="Parcourir..." 
+                    variant="solo" 
+                    accept="image/png, image/jpeg, image/webp" 
+                    multiple 
+                    chips
+                    prepend-icon="mdi-camera"
+                    v-model="files"
+                    @update:model-value="printFiles">
+      </v-file-input>
+    </div>
+
+
+    <v-card v-for="(file, index) in files">
+      <v-img
+          height="200"
+          :src="'https://picsum.photos/300/200?random=' + index"
+          cover
+          @click="removeFromFiles(file)">
+      </v-img>
+    </v-card>
+    
+  </DefaultLayout>
+</template>
+
+<script setup lang="ts">
+import DefaultLayout from '../layouts/DefaultLayout.vue';
+import { ref } from 'vue';
+
+const files = ref([]);
+
+function printFiles() {
+  console.log(files.value);
+}
+
+function removeFromFiles(file: File)
+{
+  files.value = files.value.filter((f) => f !== file);
+}
+
+</script>
+
+<style scoped>
+
+.image-input {
+  margin-top: 200px;
+}
+
+</style>
