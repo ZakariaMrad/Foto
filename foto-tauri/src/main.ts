f601f17efebf419358e1ba5f578f9b99570912df
Benjamin Lemire <benlemire@hotmail.ca>
2023-09-20 13:53:58 -0400

    test

--- foto-tauri/src/main.ts
+++ foto-tauri/src/main.ts
@@ -2,9 +2,14 @@ import { createApp } from "vue";
 import "./styles.css";
 import App from "./App.vue";
 import router from './router'
+// import { Vuetify } from 'vuetify'
+
+
 
 
 const app = createApp(App);
+
+// app.use(Vuetify);
 
 app.use(router);
 
