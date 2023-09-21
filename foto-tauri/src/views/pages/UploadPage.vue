5672596ef6f1aab5bce1ea9f98177fbdf3e7a724
KatsuroDev <katsuro.userg@gmail.com>
2023-09-21 16:39:08 -0400

    add clean list of previews

--- foto-tauri/src/views/pages/UploadPage.vue
+++ foto-tauri/src/views/pages/UploadPage.vue
@@ -1,46 +1,74 @@
 <template>
-  <DefaultLayout>
-    <div class="d-flex align-center flex-column mt-3">
-      <h1 class="text-h2 text-bold">Téléversement</h1>
-    </div>
-    <div class="w-50 mx-auto image-input">
-      <v-file-input label="Parcourir..." 
-                    variant="solo" 
-                    accept="image/png, image/jpeg, image/webp" 
-                    multiple 
-                    chips
-                    prepend-icon="mdi-camera"
-                    v-model="files"
-                    @update:model-value="printFiles">
-      </v-file-input>
-    </div>
+    <DefaultLayout>
+        <div class="d-flex align-center flex-column mt-3">
+            <h1 class="text-h2 text-bold">Téléversement</h1>
+        </div>
+        <div class="w-50 mx-auto image-input">
+            <v-file-input label="Parcourir..." 
+                        variant="solo" 
+                        accept="image/png, image/jpeg, image/webp" 
+                        multiple 
+                        chips
+                        prepend-icon="mdi-camera"
+                        v-model="files"
+                        @update:model-value="printFiles">
+            </v-file-input>
+        </div>
 
+        <v-container>
+            <v-row justify="center">
+                <v-col v-for="(file) in files">
+                    <v-card>
+                        <v-img
+                            height="200"
+                            :src="imgSrc"
+                            cover
+                            >
+                            <v-toolbar
+                                color="rgba(0, 0, 0, 0)"
+                            >
+                                <template v-slot:prepend>
+                                    <v-btn variant="tonal" icon="mdi-pencil"></v-btn>
+                                </template>
 
-    <v-card v-for="(file, index) in files">
-      <v-img
-          height="200"
-          :src="'https://picsum.photos/300/200?random=' + index"
-          cover
-          @click="removeFromFiles(file)">
-      </v-img>
-    </v-card>
-    
-  </DefaultLayout>
+                                <template v-slot:append>
+                                    <v-btn variant="tonal" icon="$close" @click="removeFromFiles(file)"></v-btn>
+                                </template>
+                            </v-toolbar>
+                        </v-img>
+                    </v-card>
+                </v-col>
+            </v-row>
+        </v-container>
+    </DefaultLayout>
 </template>
 
 <script setup lang="ts">
 import DefaultLayout from '../layouts/DefaultLayout.vue';
 import { ref } from 'vue';
 
+
+const imgSrc = ref();
 const files = ref([]);
 
 function printFiles() {
-  console.log(files.value);
+    console.log(files.value);
+    const file = files.value[0];
+    let reader = new FileReader();
+    reader.onloadend = function() {
+        imgSrc.value = reader.result?.toString();
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        imgSrc.value = "";
+    }
 }
 
 function removeFromFiles(file: File)
 {
-  files.value = files.value.filter((f) => f !== file);
+    files.value = files.value.filter((f) => f !== file);
 }
 
 </script>
@@ -48,7 +76,7 @@ function removeFromFiles(file: File)
 <style scoped>
 
 .image-input {
-  margin-top: 200px;
+  margin-top: 100px;
 }
 
 </style>
