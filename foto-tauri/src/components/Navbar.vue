63969fc60f2a7d06122ca747419544bdd3e9b5e1
ZakariaMrad <1839783@cstj.qc.ca>
2023-09-25 15:51:58 -0400

    forgot navbar

--- foto-tauri/src/components/Navbar.vue
+++ foto-tauri/src/components/Navbar.vue
@@ -1,25 +1,25 @@
 <template>
-    <header>
-        <nav class="py-2 bg-light border-bottom">
+        <header>
+            <nav class="py-2 bg-light border-bottom">
             <div class="container d-flex flex-wrap">
                 <ul class="nav me-auto">
                     <li class="nav-item">
                         <router-link class="nav-link link-dark" :to="{ name: 'home' }">Accueil</router-link>
-                    </li>
+                </li>
                     <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Features</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
+                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
                     <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
                 </ul>
                 <ul class="nav">
-                    <li class="nav-item">
+                            <li class="nav-item">
                         <button v-if="!userConnected" class="nav-link link-dark btn btn-primary" @click="activateLogin = true">Login</button>
-                        <button v-else class="nav-link link-dark btn btn-danger" @click="logout()">Logout</button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </header>
+                                <button v-else class="nav-link link-dark btn btn-danger" @click="logout()">Logout</button>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </header>
     <LoginRegister :activate="activateLogin" @isActivated="(value) => activateLogin = value" @loggedIn="userConnected=true" />
 </template>
 
