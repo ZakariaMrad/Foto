0bcf40f4796b06deb8702308ac2771bcac804808
ZakariaMrad <1839783@cstj.qc.ca>
2023-09-25 15:55:43 -0400

    forgot again

--- foto-tauri/src/components/Navbar.vue
+++ foto-tauri/src/components/Navbar.vue
@@ -1,48 +1,47 @@
 <template>
-        <header>
-            <nav class="py-2 bg-light border-bottom">
-            <div class="container d-flex flex-wrap">
-                <ul class="nav me-auto">
-                    <li class="nav-item">
-                        <router-link class="nav-link link-dark" :to="{ name: 'home' }">Accueil</router-link>
-                </li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Features</a></li>
-                <li class="nav-item"><a href="#" class="nav-link link-dark px-2">Pricing</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link link-dark px-2">FAQs</a></li>
-                    <li class="nav-item"><a href="#" class="nav-link link-dark px-2">About</a></li>
-                </ul>
-                <ul class="nav">
-                            <li class="nav-item">
-                        <button v-if="!userConnected" class="nav-link link-dark btn btn-primary" @click="activateLogin = true">Login</button>
-                                <button v-else class="nav-link link-dark btn btn-danger" @click="logout()">Logout</button>
-                        </li>
-                    </ul>
+        <v-app-bar >
+            <v-toolbar app>
+                <v-toolbar-title class="text-uppercase grey--text d-flex flex-wrap" height="auto">
+                    <span class="font-weight-bold ma-2 pa-2">Foto</span>
+                </v-toolbar-title>
+                <v-spacer></v-spacer>
+                <div class="text-center ma-2 pa-2">
+                    <v-menu open-on-hover>
+                        <template v-slot:activator="{ props }">
+                            <v-btn color="primary" v-bind="props">
+                                |||
+                            </v-btn>
+                        </template>
+
+                        <v-list>
+                            <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                                <v-list-item-title>{{ link.text }}</v-list-item-title>
+                            </v-list-item>
+                        </v-list>
+                    </v-menu>
                 </div>
-            </nav>
-        </header>
-    <LoginRegister :activate="activateLogin" @isActivated="(value) => activateLogin = value" @loggedIn="userConnected=true" />
+            </v-toolbar>
+        </v-app-bar>
 </template>
 
-<script setup lang="ts">
-import AccountRepository from '../repositories/AccountRepository'
+<script lang="ts">
 
-import { ref } from 'vue'
-import LoginRegister from './LoginRegister.vue';
-import { onMounted } from 'vue';
 
-const activateLogin = ref(false);
-const userConnected = ref<boolean>(false);
 
-const logout = async () => {
-    await AccountRepository.logout();
-    userConnected.value = false;
+export default {
+
+    data() {
+        return {
+            drawer: false,
+            links: [
+                { text: 'Dark mode', route: '' },
+                { text: `S'inscrire`, route: '' },
+                { text: 'Paramètres', route: '' },
+                { text: 'Nous contacter', route: '' },
+            ],
+        }
+    }
 }
-
-onMounted(async () => {
-    userConnected.value= await AccountRepository.isConnected();
-    
-})
-
 </script>
 
-<style scoped></style>
+
