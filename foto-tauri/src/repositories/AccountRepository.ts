5e329978c17b49e1e7c4354ed603f0cce3eb940a
KatsuroDev <katsuro.userg@gmail.com>
2023-11-11 17:34:03 -0500

    fix one repo call

--- foto-tauri/src/repositories/AccountRepository.ts
+++ foto-tauri/src/repositories/AccountRepository.ts
@@ -136,7 +136,7 @@ class AccountRepository extends Repository {
         if (!jwt.success) return { errors: jwt.errors, success: false };
 
         try {
-            const response = await client.get(`http:localhost:8000/account/${idUser}`, { responseType: ResponseType.JSON });
+            const response = await client.get(`${this.url}/account/${idUser}`, { responseType: ResponseType.JSON });
             let data = response.data as any;
             console.log(data);
 
