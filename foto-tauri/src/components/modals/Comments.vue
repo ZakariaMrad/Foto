ef1f13470e8625e96391cfe7e5af503c8fe975b1
KatsuroDev <katsuro.userg@gmail.com>
2023-12-01 20:22:10 -0500

    add comment list

--- foto-tauri/src/components/modals/Comments.vue
+++ foto-tauri/src/components/modals/Comments.vue
@@ -1,10 +1,17 @@
 <template>
     <v-dialog width="30%" v-model="props.activate">
         <v-card>
+            <v-btn style="position: absolute; top:5px; right: 5px; z-index: 10;" class="toolbar-btn" variant="tonal" color="black" icon="$close" @click="closeDialog()"></v-btn>
+            <v-list>
+                <v-list-item v-for="item in 10" prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg">
+                    <p>LOREM OASINDIOUASBNFIUBAPISUBFPAISUFJAHSFOIAHSFJASJKFHALJKSFHLASJHFLUIASHFUAHSFHNAS:OFHALSKJFNLASKJNF</p>
+                    <v-divider></v-divider>
+                </v-list-item>
+            </v-list>
             <v-card-actions>
                 <v-container>
                     <v-row>
-                        <v-avatar style="margin-top: 10px; margin-right: 10px; margin-left: 10px;" >
+                        <v-avatar style="margin-top: 10px; margin-right: 10px; margin-left: 10px;">
                             <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
                         </v-avatar>
                         <v-text-field label="Laisser un commentaire..."></v-text-field>
@@ -18,9 +25,21 @@
 
 <script setup lang="ts">
 
+import { onMounted } from 'vue';
+
+onMounted(() => {
+});
+
+const emit = defineEmits(['closeDialog'])
+
 const props = defineProps({
     activate: Boolean
 });
+
+function closeDialog()
+{
+    emit('closeDialog');
+}
 
 </script>
 
