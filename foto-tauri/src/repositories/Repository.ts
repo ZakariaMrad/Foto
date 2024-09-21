b7d187d13da924f9d0f6054018c030264f5a818e
KatsuroDev <katsuro.userg@gmail.com>
2024-09-21 12:30:20 -0400

    test

--- foto-tauri/src/repositories/Repository.ts
+++ foto-tauri/src/repositories/Repository.ts
@@ -4,7 +4,7 @@ import { JWTToken } from "../models/JWTToken";
 import AccountDatastore from "./datastore/AccountDatastore";
 
 export class Repository {
-    protected url = 'https://foto.1839783.techinfo-cstj.ca';
+    protected url = 'https://fotoapi.1929736.techinfo-cstj.ca';
     // protected url = 'http://localhost:8000';
 
     protected getAPIError(errors: any): APIError[] {
